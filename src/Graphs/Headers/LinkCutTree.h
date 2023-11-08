224a73d4b920f12d3fa44c07543e8c5c41277c13
Justice Roberts <justice.roberts@fortyau.com>
2023-11-08 12:08:26 -0600

    feat: link and cut operations on Link Cut Tree

--- src/Graphs/Headers/LinkCutTree.h
+++ src/Graphs/Headers/LinkCutTree.h
@@ -10,12 +10,14 @@ class LinkCutTree {
     Node<int, V> * root;
     std::map<V, Node<int, V> *> splayMap;
 public:
-    LinkCutTree(TreeNode<V,int> * representedTreeNode);
+    LinkCutTree(std::vector<TreeNode<V,int> *> representedForestNode);
     
     Node<int, V> * access(Node<int, V> * node);
 
     Node<int, V> * getSplayNode(V node)
         return splayMap[node];
+    Node<int, V> * link(TreeNode<V,int> * v, TreeNode<V,int> * w);
+    void cut(Node<int,V> * node);
 };
 
 // ====================================================================
@@ -23,18 +25,22 @@ public:
 // ====================================================================
 
 template<typename V>
-LinkCutTree<V>::LinkCutTree(TreeNode<V,int> * representedTree)
+LinkCutTree<V>::LinkCutTree(std::vector<TreeNode<V,int> *> representedForest)
 {
     this->splayMap = std::map<V, Node<int, V> *>();
 
     std::queue<TreeNode<V,int>*> todo = std::queue<TreeNode<V,int>>();
-    todo.push(representedTree);
+    for(auto treeRoot : representedForest)
+    {
+        todo.push(treeRoot);
+    }
     while(!todo.empty())
     {
         TreeNode<V,int> * front = todo.front();
         todo.pop();
         if(!splayMap[front->key])
             splayMap[front->key] = new Node<int,V>(front->depth, front->key);
+        
         for(auto child : front->children)
         {
             todo.push(child);
@@ -54,11 +60,47 @@ Node<int, V> * LinkCutTree<V>::access(Node<int, V> * node)
     node->right = nullptr;
 
     Node<int, v> parent = node->parent;
-    if (parent) {
+    if (parent) 
+    {
         parent->right = node;
         return access(node);
     }
     return node; // node is the root of a topmost splay tree in the Link Cut Tree.
 }
+
+template<typename V>
+Node<int, V> * LinkCutTree<V>::link(TreeNode<V,int> * v, TreeNode<V,int> * w)
+{
+    assert(w.isRoot);
+
+    Node<int, V> * vSplay = splayMap[v->key];
+    Node<int, V> * wSplay = splayMap[v->key];
+
+    assert(vSplay);
+    assert(wSplay);
+
+    vSplay = access(vSplay); // assignment technically not necessary
+    wSplay = access(wSplay);
+
+    vSplay->right = wSplay;
+    wSplay->parent = vSplay;
+
+    return vSplay;
+}
+
+template<typename V>
+void LinkCutTree<V>::cut(Node<int,V> * node)
+{
+    assert(node);
+    access(node);
+    assert(node && !node->right);
+    if(node->left)
+    {
+        node->left->parent = node->parent;
+        node->left = nullptr;
+    }
+    node->parent = nullptr;
+}
+
 
 #endif
--- src/ShannonSwitching.cpp
+++ src/ShannonSwitching.cpp
@@ -1,37 +1,43 @@
 #include "Graphs/Headers/AdjacencyLists.h"
 #include "Graphs/Headers/Graph.h"
+#include "Graphs/Headers/LinkCutTree.h"
 #include "Headers/GraphLexer.h"
 #include <iostream>
 #include <string>
 #include <cassert>
 
-int main(int argc, char ** argv) 
+int main(int argc, char ** argv)
 {
-    if(argc < 2) {
-        std::cout << "usage: ./ShannonSwitching <short | cut> < <filename>" << std::endl;
-        return 0;
-    }
-    std::string mode = argv[1];
     
-    assert(mode == "short" || mode == "cut");
-
-    AdjacencyLists<int,int> graph = AdjacencyLists<int,int>(true, true);
-
-    GraphLexer<int,int> lexer = GraphLexer<int,int>(graph);
-
-    std::cin >> lexer;
-    
-    std::pair<int,int> leastEdge(0,0); // <cost,order>
-    for(auto edge : graph.getEdges()) 
-    {
-        if( !edge.second.isProtected
-            && (leastEdge.first == 0 
-            || (edge.second.cost > 0 
-                && edge.second.cost < leastEdge.first))) 
-        {
-            leastEdge.first = edge.second.cost;
-            leastEdge.second = edge.second.order;
-        }
-    }
-    std::cout << "Selected Edge: " << leastEdge.second << " Cost: " << leastEdge.first << "\n";
 }
+
+// int main(int argc, char ** argv) 
+// {
+//     if(argc < 2) {
+//         std::cout << "usage: ./ShannonSwitching <short | cut> < <filename>" << std::endl;
+//         return 0;
+//     }
+//     std::string mode = argv[1];
+//     
+//     assert(mode == "short" || mode == "cut");
+// 
+//     AdjacencyLists<int,int> graph = AdjacencyLists<int,int>(true, true);
+// 
+//     GraphLexer<int,int> lexer = GraphLexer<int,int>(graph);
+// 
+//     std::cin >> lexer;
+//     
+//     std::pair<int,int> leastEdge(0,0); // <cost,order>
+//     for(auto edge : graph.getEdges()) 
+//     {
+//         if( !edge.second.isProtected
+//             && (leastEdge.first == 0 
+//             || (edge.second.cost > 0 
+//                 && edge.second.cost < leastEdge.first))) 
+//         {
+//             leastEdge.first = edge.second.cost;
+//             leastEdge.second = edge.second.order;
+//         }
+//     }
+//     std::cout << "Selected Edge: " << leastEdge.second << " Cost: " << leastEdge.first << "\n";
+// }
